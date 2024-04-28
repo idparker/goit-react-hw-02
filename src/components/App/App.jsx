f68b69b1ae0b6d8259074f9ed4f2dc4b3b8818aa
Vanya Drozdov <ivan.drozdov880@gmail.com>
2024-04-28 18:08:23 +0300

    code refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,58 +5,37 @@ import Feedback from "../Feedback/Feedback";
 import Notification from "../Notification/Notification";
 
 export default function App() {
-  const savedGood = parseInt(localStorage.getItem("good"));
-  const savedNeutral = parseInt(localStorage.getItem("neutral"));
-  const savedBad = parseInt(localStorage.getItem("bad"));
+  const [feedback, setFeedback] = useState({
+    good: parseInt(localStorage.getItem("good")),
+    neutral: parseInt(localStorage.getItem("neutral")),
+    bad: parseInt(localStorage.getItem("bad")),
+  });
 
-  const totalSaved = savedGood + savedBad + savedNeutral;
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round(
+    ((feedback.good + feedback.neutral) / totalFeedback) * 100
+  );
 
-  const [good, setGood] = useState(savedGood);
-  const [neutral, setNeutral] = useState(savedNeutral);
-  const [bad, setBad] = useState(savedBad);
-  const [hideFeedback, setHideFeedback] = useState(totalSaved !== 0);
-  const [showReset, setShowReset] = useState(totalSaved !== 0);
+  const handleFeedback = (type) => {
+    setFeedback((prev) => ({
+      ...prev,
+      [type]: prev[type] + 1,
+    }));
+  };
 
   const handleReset = () => {
-    setGood(0);
-    setNeutral(0);
-    setBad(0);
-
-    setHideFeedback(false);
-    setShowReset(false);
-
-    localStorage.setItem("good", 0);
-    localStorage.setItem("neutral", 0);
-    localStorage.setItem("bad", 0);
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
   };
 
-  const handleGood = () => {
-    setGood(good + 1);
-    setHideFeedback(true);
-    setShowReset(true);
-    localStorage.setItem("good", good + 1);
-  };
-
-  const handleNeutral = () => {
-    setNeutral(neutral + 1);
-    setHideFeedback(true);
-    setShowReset(true);
-    localStorage.setItem("neutral", neutral + 1);
-  };
-
-  const handleBad = () => {
-    setBad(bad + 1);
-    setHideFeedback(true);
-    setShowReset(true);
-    localStorage.setItem("bad", bad + 1);
-  };
-
-  const totalFeedback = good + neutral + bad;
-  const PositiveFeedback = Math.round(((good + neutral) / totalFeedback) * 100);
+  useEffect(() => {
+    localStorage.setItem("good", feedback.good);
+    localStorage.setItem("neutral", feedback.neutral);
+    localStorage.setItem("bad", feedback.bad);
+  }, [feedback]);
 
   useEffect(() => {
     document.title = `You clicked ${totalFeedback} times`;
-  });
+  }, [totalFeedback]);
 
   return (
     <>
@@ -66,20 +45,20 @@ export default function App() {
       />
 
       <Options
-        good={handleGood}
-        neutral={handleNeutral}
-        bad={handleBad}
-        isreset={showReset}
+        onGoodFeedback={() => handleFeedback("good")}
+        onNeutralFeedback={() => handleFeedback("neutral")}
+        onBadFeedback={() => handleFeedback("bad")}
+        feedbackCount={totalFeedback}
         reset={handleReset}
       />
 
-      {hideFeedback ? (
+      {totalFeedback > 0 ? (
         <Feedback
-          good={good}
-          neutral={neutral}
-          bad={bad}
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
           total={totalFeedback}
-          positive={PositiveFeedback}
+          positive={positiveFeedback}
         />
       ) : (
         <Notification text="No Feedback yet" />
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,12 +1,18 @@
 import css from "./Options.module.css";
 
-export default function Options({ good, neutral, bad, isreset, reset }) {
+export default function Options({
+  onGoodFeedback,
+  onNeutralFeedback,
+  onBadFeedback,
+  feedbackCount,
+  reset,
+}) {
   return (
     <div className={css.button}>
-      <button onClick={good}>Good</button>
-      <button onClick={neutral}>Neutral</button>
-      <button onClick={bad}>Bad</button>
-      {isreset && <button onClick={reset}>Reset</button>}
+      <button onClick={onGoodFeedback}>Good</button>
+      <button onClick={onNeutralFeedback}>Neutral</button>
+      <button onClick={onBadFeedback}>Bad</button>
+      {feedbackCount > 0 && <button onClick={reset}>Reset</button>}
     </div>
   );
 }
